3fee2aa182356016ec5cea12833b79f6132c8c2b
Lizard Morrow <lizardmorrow@gmail.com>
2021-08-07 11:44:33 +0100

    Write HTML Structure and select elements in JS

---
+++ assets/scripts/index.js
@@ -0,0 +1,7 @@
+const recipeName = document.querySelector('#recipe-name');
+const recipeLocation = document.querySelector('#recipe-location');
+const recipeLocOptions = document.querySelectorAll(
+  'input[name=recipe-loc-type]'
+);
+const submitButton = document.querySelector('#submit-recipe');
+const recipeList = document.querySelector('#recipe-list');
