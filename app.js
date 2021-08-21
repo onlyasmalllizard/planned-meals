f20cef88bd19b7cde7390585b2a978e1f3c4bb75
Lizard Morrow <lizardmorrow@gmail.com>
2021-08-21 16:12:06 +0100

    Render page on server

--- app.js
+++ app.js
@@ -4,13 +4,16 @@ const port = 3000;
 
 // Middleware imports
 const logger = require('morgan');
+const path = require('path');
 
 // Router imports
 const recipesRouter = require('./routes/recipes');
 
 app.use(express.json());
 app.use(logger('dev'));
+app.use(express.static(path.join(__dirname, 'public')));
 
+app.use('/', indexRouter);
 app.use('/api/recipes', recipesRouter);
 
 app.listen(port, () => {
