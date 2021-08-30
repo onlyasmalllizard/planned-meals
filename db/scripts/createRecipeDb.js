b6a83dab3d559192e005ebceb1d6c5a0c59922c4
Lizard Morrow <lizardmorrow@gmail.com>
2021-08-30 11:41:39 +0100

    Plan out database tables and update createRecipe script

--- db/scripts/createRecipeDb.js
+++ db/scripts/createRecipeDb.js
@@ -2,7 +2,7 @@ const pool = require('../index');
 
 async function createRecipeDb() {
   const queryString =
-    'CREATE TABLE IF NOT EXISTS recipes (id SERIAL PRIMARY KEY, name TEXT NOT NULL, source TEXT NOT NULL, house INTEGER, postedby INTEGER);';
+    'CREATE TABLE IF NOT EXISTS recipes (id SERIAL PRIMARY KEY, name TEXT NOT NULL, source TEXT NOT NULL, postedby INTEGER);';
   const response = await pool.query(queryString);
 
   if (response) {
