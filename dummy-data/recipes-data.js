c4cbf3a7885c7f704a120784e2203ddff80f47e8
Lizard Morrow <lizardmorrow@gmail.com>
2021-08-30 12:26:01 +0100

    Finish writing dummy data for all tables

--- dummy-data/recipes-data.js
+++ dummy-data/recipes-data.js
@@ -1,178 +1,176 @@
-const numUsers = 7;
-
 module.exports = [
   {
     name: 'Chickpea Cutlets',
     source: 'https://www.theppk.com/2010/11/doublebatch-chickpea-cutlets/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 5,
   },
   {
     name: 'Jackfruit Sabzi',
     source: 'Zaika',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 6,
   },
   {
     name: 'Singapore Noodles',
     source: 'https://thewoksoflife.com/vegetarian-singapore-noodles/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 1,
   },
   {
     name: 'King Oyster Mushroom Samgyupsal bowls',
     source: 'https://thewoksoflife.com/vegan-korean-bbq-king-oyster-mushroom/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 4,
   },
   {
     name: 'Arroz con pollo',
     source: 'Mexican cookbook',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 5,
   },
   {
     name: 'Vegan ice cream cake',
     source: 'https://sortedfood.com/recipe/12629',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 3,
   },
   {
     name: 'Vegan Adobo',
     source: 'https://thewoksoflife.com/vegan-adobo/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 3,
   },
   {
     name: 'Kalinte',
     source: 'http://www.mymoroccanfood.com/home/kalinte',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 7,
   },
   {
     name: 'Sweet Chermoula Roasted Cauliflower',
     source:
       'http://www.mymoroccanfood.com/home/sweet-chermoula-whole-roasted-cauliflower',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 8,
   },
   {
     name: 'Mushroom Stout Pie',
     source:
       'https://www.theppk.com/2013/03/mushroom-stout-pie-with-potato-biscuits/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 2,
   },
   {
     name: 'Apple Muffins',
     source: 'Fuss Free Vegan',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 1,
   },
   {
     name: 'Pizza',
     source: 'Fuss Free Vegan',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 3,
   },
   {
     name: 'Avocado Chocolate Cake',
     source:
       'https://www.ambitiouskitchen.com/best-ever-vegan-avocado-chocolate-birthday-cake-with-vegan-chocolate-ganache/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 6,
   },
   {
     name: 'Chickpea Blondies',
     source:
       'https://www.ambitiouskitchen.com/flourless-chocolate-chip-chickpea-blondies-with-sea-salt-vegan-gluten-free-healthy/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 5,
   },
   {
     name: 'Pretzels',
     source: 'https://www.loveandlemons.com/vegan-soft-baked-pretzels/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 8,
   },
   {
     name: 'Medieval Lentils',
     source: "Mum's recipe",
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 7,
   },
   {
     name: 'Date Nut Bread',
     source: 'Joy of Cooking',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 3,
   },
   {
     name: 'Seitan Steak',
     source:
       'http://www.vegansandra.com/2015/11/perfect-homemade-seitan-steaks.html',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 1,
   },
   {
     name: 'Banoffee Pie',
     source: 'Vegan Pies book',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 5,
   },
   {
     name: 'Sweet and Sour Cabbage',
     source:
       'http://www.vegansandra.com/2017/05/sweet-and-sour-chinese-cabbage.html',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 6,
   },
   {
     name: 'Challah',
     source: 'Vegan Holiday Cookbook',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 4,
   },
   {
     name: 'Boston Cream Doughnuts',
     source: 'http://www.marystestkitchen.com/vegan-boston-cream-doughnuts/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 2,
   },
   {
     name: 'Vegan Gefilte Fish',
     source: 'https://thevword.net/2013/03/vegan-gefilte-fish.html',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 6,
   },
   {
     name: 'Cauliflower steak with tahini',
     source:
       'https://www.myjewishlearning.com/the-nosher/cauliflower-steak-with-tahini-and-pine-nuts-recipe/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 3,
   },
   {
     name: 'Cannellini pasta sauce',
     source:
       'https://www.matchingfoodandwine.com/news/recipes/creamy-cannellini-and-fennel-pasta/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 8,
   },
   {
     name: 'Brownies',
     source: 'https://www.noracooks.com/vegan-brownies-recipe/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 7,
   },
   {
     name: 'Salt and pepper tofu',
     source: 'https://thewoksoflife.com/salt-and-pepper-tofu/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 1,
   },
   {
     name: 'Peanut Noodles',
     source: 'https://thewoksoflife.com/peanut-noodles/#recipe',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 3,
   },
   {
     name: 'Croissants',
     source: 'http://www.veganbaking.net/recipes/pastries/croissants',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 2,
   },
   {
     name: 'Kung Pao Tofu',
     source: 'https://thewoksoflife.com/kung-pao-tofu-vegan-vegetarian/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 6,
   },
   {
     name: 'Saffron Chorba',
     source:
       'http://www.mymoroccanfood.com/home/saffron-vegan-chorba-with-roasted-aubergine-and-sweet-potatoes',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 4,
   },
   {
     name: 'Sesame Noodles',
     source: 'https://thewoksoflife.com/sesame-noodles/',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 3,
   },
   {
     name: 'Lentil Stew',
     source: 'http://www.mymoroccanfood.com/home/vegetarian-lentil-stew',
-    postedby: Math.floor(Math.random() * numUsers),
+    postedby: 6,
   },
 ];
---
+++ dummy-data/users-data.js
@@ -0,0 +1,10 @@
+module.exports = [
+  { name: 'Biscuits', recipes: [3, 11, 18, 27], household: 1 },
+  { name: 'Nori', recipes: [10, 22, 29], household: 1 },
+  { name: 'Hippo', recipes: [6, 7, 12, 17, 24, 28, 32], household: 2 },
+  { name: 'Syd', recipes: [4, 21, 31], household: 2 },
+  { name: 'Balthazar', recipes: [1, 5, 14, 19], household: 2 },
+  { name: 'Fennel', recipes: [2, 13, 20, 23, 30, 33], household: 3 },
+  { name: 'Sprocket', recipes: [8, 16, 26], household: 3 },
+  { name: 'Ghost', recipes: [9, 15, 25], household: 3 },
+];
